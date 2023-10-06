7497441eaff7d92ad92308e01edfb60dff603c94
root <scjjb@leeds.ac.uk>
2023-10-06 09:05:10 +0000

    rerun HIPT-ABMIL tuning with HIPT pretraining working

--- main.py
+++ main.py
@@ -79,6 +79,13 @@ def main():
                 }
         else:
             if args.no_inst_cluster:
+                    ## ATEC23 HIPT-ABMIL first tuning
+                    "A_model_size": tune.grid_search(["hipt_medium","hipt_small","hipt_smaller"]),
+                    "lr": tune.grid_search([0.001,0.0001,0.00001]),
+                    "patches": tune.grid_search([25,50, 75]),
+                    "drop_out": tune.grid_search([0.25,0.5, 0.75]),
+                    "reg": tune.grid_search([0.01, 0.001, 0.0001]),
+
                     ## StagingVsIDS first tuning - just getting a grasp on lr and patches, will look at others later
                     #search_space={
                     #        "reg": tune.grid_search([0.0001]),
@@ -195,13 +202,13 @@ def main():
 
                     ## HIPT_ABMIL ESGO staging data fifth
                     # hipt_mega_mega2 expands final dimension instead of intermediate
-                    search_space={
-                            "reg": tune.grid_search([0.0001]),
-                            "drop_out": tune.grid_search([0.25,0.5,0.75]),
-                            "lr": tune.grid_search([0.0001,0.00001,0.000001]),
-                            "patches": tune.grid_search([80,40]),
-                            "A_model_size": tune.grid_search(["hipt_mega_mega2"])
-                            }
+                    #search_space={
+                    #        "reg": tune.grid_search([0.0001]),
+                    #        "drop_out": tune.grid_search([0.25,0.5,0.75]),
+                    #        "lr": tune.grid_search([0.0001,0.00001,0.000001]),
+                    #        "patches": tune.grid_search([80,40]),
+                    #        "A_model_size": tune.grid_search(["hipt_mega_mega2"])
+                    #        }
             else:
                 if args.model_size in ["hipt_big","hipt_medium","hipt_small","hipt_smaller","hipt_smallest"]:
                     search_space={
