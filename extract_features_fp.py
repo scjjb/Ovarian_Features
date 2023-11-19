3ebd3808b28816abd64bb9080f53a74d8dcc5f17
root <scjjb@leeds.ac.uk>
2023-11-19 16:18:44 +0000

    add timing to feature extraction

--- extract_features_fp.py
+++ extract_features_fp.py
@@ -229,6 +229,7 @@ if __name__ == '__main__':
         total = len(bags_dataset)
 
         unavailable_patch_files=0
+        total_time_elapsed = 0.0
         for bag_candidate_idx in range(total):
             print('\nprogress: {}/{}'.format(bag_candidate_idx, total))
             print('skipped unavailable slides: {}'.format(unavailable_patch_files))
@@ -257,9 +258,10 @@ if __name__ == '__main__':
                 time_start = time.time()
                 wsi = openslide.open_slide(slide_file_path)
                 output_file_path = compute_w_loader(h5_file_path, output_path, wsi, 
-                model = model, batch_size = args.batch_size, verbose = 1, print_every = 20, 
+                model = model, batch_size = args.batch_size, verbose = 1, print_every = 100, 
                 custom_downsample=args.custom_downsample, target_patch_size=args.target_patch_size)
                 time_elapsed = time.time() - time_start
+                total_time_elapsed += time_elapsed
                 print('\ncomputing features for {} took {} s'.format(output_file_path, time_elapsed))
                 file = h5py.File(output_file_path, "r")
 
@@ -275,3 +277,4 @@ if __name__ == '__main__':
                 print("patch file unavailable")
                 continue
         print("finished running with {} unavailable slide patch files".format(unavailable_patch_files))
+        print("total time: {}".format(total_time_elapsed))
