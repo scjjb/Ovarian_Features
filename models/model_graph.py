79ab2ec8738cd948452b74b87fa3dc02ecae817f
root <scjjb@leeds.ac.uk>
2024-01-11 11:14:37 +0000

    change graph_ms to only a single dropout layer before any linear layers

--- models/model_graph.py
+++ models/model_graph.py
@@ -71,10 +71,9 @@ class Graph_Model(torch.nn.Module):
                     xhidden = xhidden + torch.cat([gmp(x,batch), gap(x,batch)], dim=1)
 
         x = xhidden 
+        x = F.dropout(x, p=self.drop_out, training=training)
         x = F.relu(self.lin1(x))
-        x = F.dropout(x, p=self.drop_out, training=training)
         x = F.relu(self.lin2(x))
-        x = F.dropout(x, p=self.drop_out, training=training)
         x = self.lin3(x)
 
         logits = x
