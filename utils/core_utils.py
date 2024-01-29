e8a36caf1655c33679248e97f0c87c3ba1091b04
scjjb <jbreen8@hotmail.co.uk>
2024-01-29 22:28:48 +0000

    even less intense lr decay

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -308,7 +308,7 @@ def train(config, datasets, cur, class_counts_train, class_counts_val, args):
     if args.model_type in ['clam_sb', 'clam_mb'] and not args.no_inst_cluster:
         use_clam = True
 
-    scheduler = ReduceLROnPlateau(optimizer, 'min',factor=0.25, patience=15, verbose = True)
+    scheduler = ReduceLROnPlateau(optimizer, 'min',factor=0.5, patience=10, verbose = True)
     
     for epoch in range(args.max_epochs):
         ## train a loop and evaluate validation set
