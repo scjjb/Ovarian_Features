27af533b81e0dbff07a827bc5a49de9773f600a8
root <scjjb@leeds.ac.uk>
2024-04-11 10:55:39 +0000

    add vitl to heatmaps

--- create_heatmaps.py
+++ create_heatmaps.py
@@ -26,7 +26,7 @@ from datasets.dataset_h5 import eval_transforms
 import timm
 
 parser = argparse.ArgumentParser(description='Heatmap inference script')
-parser.add_argument('--model', type=str, default='resnet50',choices=['resnet50','uni','hipt'])
+parser.add_argument('--model', type=str, default='resnet50',choices=['resnet50','uni','vitl','hipt'])
 parser.add_argument('--save_exp_code', type=str, default=None,
                                         help='experiment code')
 parser.add_argument('--overlap', type=float, default=None)
@@ -179,6 +179,13 @@ if __name__ == '__main__':
             feature_extractor = timm.create_model("vit_large_patch16_224", img_size=224, patch_size=16, init_values=1e-5, num_classes=0, dynamic_img_size=True)
             local_dir = "/mnt/results/vit_large_patch16_224.dinov2.uni_mass100k/"
             feature_extractor.load_state_dict(torch.load(os.path.join(local_dir, "pytorch_model.bin"), map_location="cpu"), strict=True)
+            transforms = transforms.Compose(
+                    [transforms.Resize(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+        elif model_type=='vitl':
+            print("CREATING HEATMAP USING VITL")
+            feature_extractor = timm.create_model("vit_large_patch16_224",  num_classes=0,  pretrained = True)
             transforms = transforms.Compose(
                     [transforms.Resize(224),
                     transforms.ToTensor(),
